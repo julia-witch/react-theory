3a8cedd109b3927775780d305070e9b6f7d92dad
“Julia <julia1603@gmail.com>
2020-11-14 19:28:39 +0300

    first commit

---
+++ src/Car/Car.js
@@ -0,0 +1,24 @@
+import React from 'react';
+/*function car(){
+    return (
+        <div>This is car component</div>
+    )
+}*/
+
+/*const car = () => {
+    return (
+        <div>This is car component</div>
+    )
+}*/
+
+/*const car = () => (
+    <div>This is car
+        <strong>component</strong>
+    </div>
+)*/
+
+export default ( )=> (
+    <div>This is car
+        <strong>component</strong>
+    </div>
+)
